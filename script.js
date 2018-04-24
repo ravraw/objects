bbb31706a0c1561a27aeb7d2fcafe3535370c23f
ravindra rawat <ravinrawat.wd@gmail.com>
2018-04-24 23:23:00 +0000

    Object.create

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 // Function constructer
 
-const Person = function(name, yearOfBirth, job) {
+/*const Person = function(name, yearOfBirth, job) {
   (this.name = name), (this.yearOfBirth = yearOfBirth), (this.job = job);
 };
 
@@ -13,3 +13,23 @@ Person.prototype.calculateAge = function() {
 var john = new Person("John", 1992, "Teacher");
 var jane = new Person("Jane", 1982, "Doctor");
 var mark = new Person("Mark", 1955, "Retired");
+*/
+
+// Object.create
+
+var personProto = {
+  calculateAge: function() {
+    console.log(2016 - this.yearOfBirth);
+  }
+};
+
+var john = Object.create(personProto);
+john.name = "John";
+john.yearOfBirth = 1990;
+john.job = "teacher";
+
+var jane = Object.create(personProto, {
+  name: { value: "Jane" },
+  yearofBirth: { value: 1969 },
+  job: { value: "designer" }
+});
