60e5e9d42b9d9bde4c0c99ce0683e14d319961a6
ravindra rawat <ravinrawat.wd@gmail.com>
2018-04-25 20:28:08 +0000

    palindrome

--- script.js
+++ script.js
@@ -76,5 +76,92 @@ function interviewQuestion(job) {
   };
 }
 
-const teacherQuestion = interviewQuestion("teacher");
-teacherQuestion("john");
+//const teacherQuestion = interviewQuestion("teacher");
+//teacherQuestion("john");
+
+//// closure
+
+var counter = (function() {
+  var privateCounter = 0;
+  function changeBy(val) {
+    privateCounter += val;
+  }
+  return {
+    increment: function() {
+      changeBy(1);
+    },
+    decrement: function() {
+      changeBy(-1);
+    },
+    value: function() {
+      return privateCounter;
+    }
+  };
+})();
+
+// console.log(counter.value()); // logs 0
+// counter.increment();
+// counter.increment();
+// console.log(counter.value()); // logs 2
+// counter.decrement();
+// console.log(counter.value()); // logs 1
+
+////  Makecounter
+
+var makeCounter = function() {
+  var privateCounter = 0;
+  function changeBy(val) {
+    privateCounter += val;
+  }
+  return {
+    increment: function() {
+      changeBy(1);
+    },
+    decrement: function() {
+      changeBy(-1);
+    },
+    value: function() {
+      return privateCounter;
+    }
+  };
+};
+
+// var counter1 = makeCounter();
+// var counter2 = makeCounter();
+// alert(counter1.value()); /* Alerts 0 */
+// counter1.increment();
+// counter1.increment();
+// alert(counter1.value()); /* Alerts 2 */
+// counter1.decrement();
+// alert(counter1.value()); /* Alerts 1 */
+// alert(counter2.value()); /* Alerts 0 */
+
+//question 1 :
+
+//Answer
+for (var i = 0; i < 5; i++) {
+  let x = i;
+  let btn = document.createElement("button");
+  btn.appendChild(document.createTextNode("Button " + i));
+  btn.addEventListener("click", function() {
+    console.log(x);
+  });
+  document.body.appendChild(btn);
+}
+
+// is Palindrome
+
+var isPalindrome = function(str) {
+  var strLen = str.length;
+  if (strLen === 0 || strLen === 1) {
+    return true;
+  }
+
+  if (str[0] === str[strLen - 1]) {
+    return isPalindrome(str.slice(1, strLen - 1));
+  }
+
+  return false;
+};
+
+console.log(isPalindrome("abcdefggfedcba"));
