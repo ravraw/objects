b0a36f3edd8321d36a052a47be624b901160bb36
ravindra rawat <ravinrawat.wd@gmail.com>
2018-04-25 17:27:27 +0000

    closures

--- script.js
+++ script.js
@@ -33,3 +33,48 @@ var jane = Object.create(personProto, {
   yearofBirth: { value: 1969 },
   job: { value: "designer" }
 });
+
+// callback function : return
+
+// function interviewQuestion(job) {
+//   if (job === "teacher") {
+//     return function(name) {
+//       console.log(name + " " + "What do you teach ?");
+//     };
+//   } else if (job === "designer") {
+//     return function(name) {
+//       console.log(name + " " + ", can you please explain UX design");
+//     };
+//   } else {
+//     return function(name) {
+//       console.log(name + " " + ",what do you do ?");
+//     };
+//   }
+// }
+
+// const teacherQuestion = interviewQuestion("teacher");
+// console.log(teacherQuestion("john"));
+
+/// closure
+
+function interviewQuestion(job) {
+  let question = "";
+
+  switch (job) {
+    case "teacher":
+      question = " " + "What do you teach ?";
+      break;
+    case "designer":
+      question = " " + ", can you please explain UX design";
+      break;
+    default:
+      question = " " + ",what do you do ?";
+  }
+
+  return function(name) {
+    console.log(name + question);
+  };
+}
+
+const teacherQuestion = interviewQuestion("teacher");
+teacherQuestion("john");
